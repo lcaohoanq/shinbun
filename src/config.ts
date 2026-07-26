ba67f45c3070b3f0c00aedcd1c86d481aa217e03
lcaohoanq <hoangclw@gmail.com>
2026-07-26 16:42:00 +0700

    style: update avatar

--- src/config.ts
+++ src/config.ts
@@ -58,7 +58,7 @@ export const navBarConfig: NavBarConfig = {
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/cropped.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	avatar: "assets/images/avatar_2.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "Thợ Dev",
 	bio: "Software developer",
 	/*
